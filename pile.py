abb574ffdf652496cd412d520c71c0336a953bd6
William Lafontaine <wil90_999@hotmail.com>
2018-04-05 10:53:57 -0400

    c'est ça

---
+++ pile.py
@@ -0,0 +1,21 @@
+#William Lafontaine
+#class pile (stack)
+#2 Avril 2018
+class Pile(object):
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
+
+    def size(self):
+        return len(self.items)
